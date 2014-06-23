e5954d07886d20eb6061ed3ed1457a3029ad8111
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-06-23 20:00:37 +0530

    Added a telecore.h file. This file will contain our custom functions.

---
+++ telecore.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   telecore.h
+ * Author: ajay
+ *
+ * Created on 23 June, 2014, 7:50 PM
+ */
+
+#ifndef TELECORE_H
+#define	TELECORE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void display_menu();
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TELECORE_H */
+
