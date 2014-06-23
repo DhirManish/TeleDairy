6537b978e64a40653c1cffbf80c452547a7b8cdf
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-06-23 19:19:28 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: ajay
+ *
+ * Created on 23 June, 2014, 7:10 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    return (EXIT_SUCCESS);
+}
+
