326c24916b3cbdf83b0c4634d0585d0e3b8078d3
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-06-23 20:01:17 +0530

    Added definition to display_menu() declared in telecore.h to telecore.c

---
+++ telecore.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   main.c
+ * Author: ajay
+ *
+ * Created on 23 June, 2014, 7:50 PM
+ */
+
+#include <stdio.h>
+#include "telecore.h"
+
+void display_menu()
+{
+    printf("%40s\n", "MENU");
+    printf("%40s\n", "====");
+}
