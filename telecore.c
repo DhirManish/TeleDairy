10c500e04129a5234210917d2ab4573bad3342cb
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-06-23 20:41:09 +0530

    Added definition to getche() and called it in display_menu()

--- telecore.c
+++ telecore.c
@@ -6,9 +6,25 @@
  */
 
 #include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 #include "telecore.h"
 
-int display_menu()
+/* reads from keypress, echoes */
+int getche(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+
+int display_menu(void)
 {
     printf("\t\t\t%s\n", "MENU");
     printf("\t\t\t%s\n", "====");
@@ -16,5 +32,8 @@ int display_menu()
     printf("\t\t%s\n", "2. Remove an entry");
     printf("\t\t%s\n", "3. Edit an entry");
     printf("\t\t%s\n", "4. Display an entry"); 
-    printf("\t\t%s\n", "5. Exit");    
+    printf("\t\t%s\n", "5. Exit");
+    printf("\t%30s", "Choose your option(1-5): ");
+    
+    return getche();
 }
